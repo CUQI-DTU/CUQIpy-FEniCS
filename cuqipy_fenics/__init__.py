7bead9e079ffa770fb6d0eec63b25bff2e85fe7e
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-09-22 10:48:18 +0200

    Append geometry.FEniCSContinuous to the _identity_geometries list

--- cuqipy_fenics/__init__.py
+++ cuqipy_fenics/__init__.py
@@ -7,3 +7,8 @@ dl.set_log_active(False)
 
 from . import _version
 __version__ = _version.get_versions()['version']
+
+#TODO: This logic need to be removed after _get_identity_geometries 
+# is removed from cuqipy 
+from cuqi.geometry import _identity_geometries
+_identity_geometries.append(geometry.FEniCSContinuous)
