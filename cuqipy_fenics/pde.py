b2023d1ed348601d668e710b417effb39308ff4e
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-09-24 12:39:40 +0200

    Add documentation

--- cuqipy_fenics/pde.py
+++ cuqipy_fenics/pde.py
@@ -11,6 +11,47 @@ __all__ = [
 ]
 
 class FEniCSPDE(PDE,ABC):
+    """ Base class that represents PDEs problem defined in FEniCS. This class is not meant to be used directly,
+    but rather it defines the API that should be implemented by subclasses for specific types of PDEs.
+
+    This class along with the `cuqipy_fenics.geometry`, interfaces FEniCS PDE models with CUQIpy library.
+
+    Parameters
+    ----------
+    PDE_form : python function handle
+        Function handle of a python function that returns the FEniCS weak form of the PDE.
+
+        This python function takes as input, in this given order, the Bayesian parameter (input of the forward model), the state variable (solution variable), the adjoint variable (which is also the test variable in the weak formulation) as FEniCS functions (or FEniCS trail or test functions). See, for example, `demos/demo03_poisson_circular.py` for an example of how to define this function.
+
+    mesh : FEniCS mesh
+        FEniCS mesh object that defines the discretization of the domain.
+
+    solution_function_space : FEniCS function space
+        FEniCS function space object that defines the function space of the state variable (solution variable).
+
+    parameter_function_space : FEniCS function space
+        FEniCS function space object that defines the function space of the Bayesian parameter (input of the forward model).
+
+    dirichlet_bc : FEniCS Dirichlet boundary condition object
+        FEniCS Dirichlet boundary condition object that defines the Dirichlet boundary conditions of the PDE.
+
+    adjoint_dirichlet_bc : FEniCS Dirichlet boundary condition object, required if the gradient is to be computed
+        FEniCS Dirichlet boundary condition object that defines the Dirichlet boundary conditions of the adjoint PDE.
+
+    observation_operator : python function handle, optional
+        Function handle of a python function that returns the observed quantity from the PDE solution. If not provided, the identity operator is assumed (i.e. the entire solution is observed).
+        This python function takes as input the Bayesian parameter (input of the forward model) and the state variable (solution variable) as first and second inputs, respectively.
+
+        The returned observed quantity can be a ufl.algebra.Operator, FEniCS Function, np.ndarray, int, or float.
+
+        Examples of `observation_operator` are `lambda m, u: m*u` and `lambda m, u: m*dl.sqrt(dl.inner(dl.grad(u),dl.grad(u)))`
+
+    Example
+    --------
+    See `demos/demo03_poisson_circular.py` for an example of how to define a `cuqipy_fenics.pde` objects.
+
+    """
+
     def __init__(self, PDE_form, mesh, solution_function_space, parameter_function_space, dirichlet_bc, adjoint_dirichlet_bc=None, observation_operator=None):
         self.PDE_form = PDE_form # function of PDE_solution, PDE_parameter, test_function
         self.mesh = mesh 
@@ -27,7 +68,7 @@ class FEniCSPDE(PDE,ABC):
     
     @parameter.setter
     def parameter(self, value):
-        """ Set the parameter of the PDE """
+        """ Set the parameter of the PDE. Since the PDE solution depends on the parameter, this will set the PDE solution to None. """
         self._parameter = value
         self._forward_solution = None
 
@@ -66,27 +107,7 @@ class FEniCSPDE(PDE,ABC):
         raise NotImplementedError
 
 class SteadyStateLinearFEniCSPDE(FEniCSPDE):
-def __init__(
-    self,
-    PDE_form,
-    mesh,
-    solution_function_space,
-    parameter_function_space,
-    dirichlet_bc,
-    adjoint_dirichlet_bc=None,
-    observation_operator=None,
-):
-    super().__init__(
-        PDE_form,
-        mesh,
-        solution_function_space,
-        parameter_function_space,
-        dirichlet_bc,
-        adjoint_dirichlet_bc,
-        observation_operator=observation_operator,
-    )
-
-
+    """ Class representation of steady state linear PDEs defined in FEniCS. It accepts the same arguments as the base class `cuqipy_fenics.pde.FEniCSPDE`."""
 
     def assemble(self, parameter=None):
         if parameter is not None:
@@ -115,7 +136,13 @@ def __init__(
 
         Note: This implementation is largely based on the code:
         https://github.com/hippylib/hippylib/blob/master/hippylib/modeling/PDEProblem.py
+
+        See also: Gunzburger, M. D. (2002). Perspectives in flow control and optimization. Society for Industrial and Applied Mathematics, for adjoint based derivative derivation. 
         """
+        # Raise an error if the adjoint boundary conditions are not provided
+        if self.adjoint_dirichlet_bc is None:
+            raise ValueError("The adjoint Dirichlet boundary conditions are not defined.")
+
         # Create needed functions
         trial_adjoint = dl.TrialFunction(self.solution_function_space)
         adjoint = dl.Function(self.solution_function_space)
@@ -129,26 +156,27 @@ def __init__(
         test_parameter = dl.TestFunction(self.parameter_function_space)
         test_solution = dl.TestFunction(self.solution_function_space)
         
-        J = self.PDE_form(wrt, self.forward_solution, trial_adjoint)
-        dJdu = dl.derivative(J, self.forward_solution, test_solution)
+        temp_form = self.PDE_form(wrt, self.forward_solution, trial_adjoint) # weak form used for building the adjoint operator
+        adjoint_form = dl.derivative(temp_form, self.forward_solution, test_solution)
         
-dJdu_matrix, _ = dl.assemble_system(
-    dJdu,
-    ufl.inner(self.forward_solution, test_solution) * ufl.dx,
-    self.adjoint_dirichlet_bc,
-)
+        adjoint_matrix, _ = dl.assemble_system(
+            adjoint_form,
+            ufl.inner(self.forward_solution, test_solution) * ufl.dx,
+            self.adjoint_dirichlet_bc,
+        )
 
         #TODO: account for observation operator
         if self.observation_operator is not None:
             raise NotImplementedError("Gradient wrt parameter for PDE with observation operator not implemented")
             
         adjoint_rhs = -direction.vector() 
-        dl.solve(dJdu_matrix, adjoint.vector(), adjoint_rhs)
+        dl.solve(adjoint_matrix, adjoint.vector(), adjoint_rhs)
 
         # Compute gradient
-        J2 = self.PDE_form(wrt, self.forward_solution, adjoint)
+        temp_form = self.PDE_form(wrt, self.forward_solution, adjoint) # weak form used for building the gradient
+        gradient_form = dl.derivative(temp_form, wrt, test_parameter)
         gradient = dl.Function(self.parameter_function_space)
-        dl.assemble(dl.derivative(J2, wrt, test_parameter), tensor=gradient.vector())
+        dl.assemble(gradient_form, tensor=gradient.vector())
         return gradient
 
 
