21074be89ccdbd6d25978ab5120fdb2743f38fa1
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-10-12 15:35:17 +0200

    update test

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -5,8 +5,8 @@ def test_dolfin_mesh():
     """Test creating a MaternExpansion geometry"""
     mesh = dl.UnitSquareMesh(20,20)
     V = dl.FunctionSpace(mesh, 'CG', 1)
-    geometry = cuqipy_fenics.FEniCSContinuous(V)
-    MaternGeometry = cuqipy_fenics.MaternExpansion(geometry, 
+    geometry = cuqipy_fenics.geometry.FEniCSContinuous(V)
+    MaternGeometry = cuqipy_fenics.geometry.MaternExpansion(geometry, 
                                     length_scale = .2,
                                     num_terms=128)
     assert(True)
