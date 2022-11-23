fac4869f44c7195f9d166f7da54ee893a366b483
amal-ghamdi <amal.m.alghamdi@gmail.com>
2022-11-23 07:03:18 +0100

    Add tests

--- tests/test_PDEModel.py
+++ tests/test_PDEModel.py
@@ -3,6 +3,7 @@ import cuqi
 import cuqipy_fenics
 import numpy as np
 import pytest
+import time
 
 
 def test_model_input():
@@ -35,3 +36,295 @@ def test_model_input():
     u = dl.Function(V)
     with pytest.raises(AttributeError):
         y = model(u)
+
+
+def test_solver_choice():
+    """Test passing different solvers to PDEModel"""
+    # Create the variational problem
+    poisson = Poisson()
+
+    # Set up model parameters
+    m = dl.Function(poisson.parameter_function_space)
+    m.vector()[:] = 1.0
+
+    # Create a PDE object with Krylov solver
+    PDE_with_solver1 = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE(
+        poisson.form,
+        poisson.mesh,
+        poisson.solution_function_space,
+        poisson.parameter_function_space,
+        poisson.bc,
+        linalg_solve=dl.KrylovSolver())
+
+    # Solve the PDE
+    PDE_with_solver1.assemble(m)
+    u1, info = PDE_with_solver1.solve()
+
+    # Create a PDE object with direct LU solver
+    PDE_with_solver2 = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE(
+        poisson.form,
+        poisson.mesh,
+        poisson.solution_function_space,
+        poisson.parameter_function_space,
+        poisson.bc,
+        linalg_solve=dl.LUSolver())
+
+    # Solve the PDE
+    PDE_with_solver2.assemble(m)
+    u2, info = PDE_with_solver2.solve()
+
+    # Check that the solutions are the same
+    assert np.allclose(u1.vector().get_local(), u2.vector().get_local())
+
+
+def test_reuse_assembled():
+    """Test that reusing the assembled and refactored lhs gives the same solution
+     and a better performance"""
+    # Create the variational problem
+    poisson = Poisson()
+    poisson.mesh = dl.UnitSquareMesh(50, 50)
+
+    # Set up model parameters
+    m = dl.Function(poisson.parameter_function_space)
+    m.vector()[:] = 1.0
+
+    # Create a PDE object, generate an error if the full form is provided
+    # but reuse_assembled is True
+    with pytest.raises(ValueError):
+        PDE = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE(
+            poisson.form,
+            poisson.mesh,
+            poisson.solution_function_space,
+            poisson.parameter_function_space,
+            poisson.bc,
+            reuse_assembled=True)
+
+    # Create a PDE object
+    PDE = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE(
+        (poisson.lhs_form, poisson.rhs_form),
+        poisson.mesh,
+        poisson.solution_function_space,
+        poisson.parameter_function_space,
+        poisson.bc,
+        reuse_assembled=True)
+
+    # Solve the PDE
+    PDE.assemble(m)
+    u1, info = PDE.solve()
+
+    # update the parameter and solve again
+    sin_func = dl.Expression("sin(2*pi*x[0])", degree=1)
+    m.interpolate(sin_func)
+    t0 = time.time()
+    PDE.assemble(m)
+    u1, info = PDE.solve()
+    t1 = time.time()
+    t_first = t1 - t0
+
+    # Solve the PDE again
+    t0 = time.time()
+    PDE.assemble(m)
+    u2, info = PDE.solve()
+    t1 = time.time()
+    t_refactor = t1 - t0
+
+    # Check that the solutions are the same
+    assert np.allclose(u1.vector().get_local(), u2.vector().get_local())
+
+    # Check that using the reuse_assembled option is faster
+    assert t_refactor < 0.25*t_first
+
+
+def test_form():
+    """Test creating PDEModel with full form, and with lhs and rhs forms"""
+    # Create the variational problem
+    poisson = Poisson()
+
+    # Set up model parameters
+    m = dl.Function(poisson.parameter_function_space)
+    m.vector()[:] = 1.0
+
+    # Create a PDE object with full form
+    PDE_with_full_form = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE(
+        poisson.form,
+        poisson.mesh,
+        poisson.solution_function_space,
+        poisson.parameter_function_space,
+        poisson.bc)
+
+    # Solve the PDE
+    PDE_with_full_form.assemble(m)
+    u1, info = PDE_with_full_form.solve()
+
+    # Create a PDE object with lhs and rhs forms
+    PDE_with_lhs_rhs_forms = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE(
+        (poisson.lhs_form, poisson.rhs_form),
+        poisson.mesh,
+        poisson.solution_function_space,
+        poisson.parameter_function_space,
+        poisson.bc)
+
+    # Solve the PDE
+    PDE_with_lhs_rhs_forms.assemble(m)
+    u2, info = PDE_with_lhs_rhs_forms.solve()
+
+    # Check that the solutions are the same
+    assert np.allclose(u1.vector().get_local(), u2.vector().get_local())
+
+def test_shallow_copy(copy_reference):
+
+    poisson1 = Poisson()
+    poisson1.mesh = dl.UnitSquareMesh(40, 40)
+    poisson1.f = dl.Constant(1.0)
+
+    poisson2 = Poisson()
+    poisson2.mesh = poisson1.mesh
+    poisson2.f = dl.Expression("sin(2*x[0]*pi)*sin(2*x[1]*pi)", degree=1)
+
+    def u_boundary(x, on_boundary):
+        return on_boundary and ( x[0] < dl.DOLFIN_EPS or x[0] > 1.0 - dl.DOLFIN_EPS)
+
+    poisson1.bc = dl.DirichletBC(poisson1.solution_function_space, dl.Constant(0.0), u_boundary)
+    poisson2.bc = poisson1.bc
+
+  
+    #%% 2. Set up CUQI PDE Bayesian problem  
+    #%% 2.1. Create two PDE objects with different source terms 
+    PDE1 = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE( 
+        (poisson1.lhs_form, poisson1.rhs_form),
+        poisson1.mesh, 
+        parameter_function_space=poisson1.parameter_function_space,
+        solution_function_space=poisson1.solution_function_space,
+        dirichlet_bc=poisson1.bc,
+        observation_operator=None,
+        reuse_assembled=False)
+
+    PDE2 = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE( 
+        (poisson2.lhs_form, poisson2.rhs_form),
+        poisson2.mesh, 
+        parameter_function_space=poisson2.parameter_function_space,
+        solution_function_space=poisson2.solution_function_space,
+        dirichlet_bc=poisson2.bc,
+        observation_operator=None,
+        reuse_assembled=False)    
+
+    
+    #%% 2.2. Create domain geometry 
+    fenics_continuous_geo = cuqipy_fenics.geometry.FEniCSContinuous(poisson1.parameter_function_space)
+    domain_geometry = cuqipy_fenics.geometry.MaternExpansion(fenics_continuous_geo, length_scale = .1, num_terms=32)
+    
+    #%% 2.3. Create range geometry
+    range_geometry= cuqipy_fenics.geometry.FEniCSContinuous(poisson1.solution_function_space)
+    
+    #%% 2.4. Create cuqi forward model (two models corresponding to two PDE objects)
+    cuqi_model1 = cuqi.model.PDEModel(PDE1, domain_geometry =domain_geometry,range_geometry=range_geometry)
+    
+    cuqi_model2 = cuqi.model.PDEModel(PDE2, domain_geometry =domain_geometry,range_geometry=range_geometry)
+    
+    #%% 2.5. Create the prior
+    x = cuqi.distribution.Gaussian(mean=np.zeros(cuqi_model1.domain_dim),
+                                   cov=1, geometry=domain_geometry)
+    
+    
+    #%% 2.6. Create exact solution and data
+    np.random.seed(0)
+    exact_solution =cuqi.samples.CUQIarray( np.random.randn(domain_geometry.par_dim),is_par=True,geometry= domain_geometry )
+    
+    data1 = cuqi_model1(exact_solution)
+    data2 = cuqi_model2(exact_solution)
+    
+    #%% 2.7. Create likelihood 1
+    y1 = cuqi.distribution.Gaussian(mean=cuqi_model1(x),
+                                   cov=np.ones(cuqi_model1.range_dim)*.01**2)
+    y1 = y1(y1= data1)
+    
+    #%% 2.9. Create likelihood 2
+    y2 = cuqi.distribution.Gaussian(mean=cuqi_model2(x),
+                                     cov=np.ones(cuqi_model2.range_dim)*.01**2) 
+    y2 = y2(y2= data2)
+    
+    #%% 2.10. Create posterior
+    cuqi_posterior = cuqi.distribution.JointDistribution( y1, y2, x)._as_stacked()
+    
+    
+    #%% 3 Solve the Bayesian problem
+    #%% 3.1. Sample the posterior (Case 1: no reuse of assembled operators)
+    Ns = 10
+    np.random.seed(0) # fix seed for reproducibility when setting reuse_assembled=True
+    sampler = cuqi.sampler.MetropolisHastings(cuqi_posterior)
+    t0 = time.time()
+    samples1 = sampler.sample_adapt(Ns,Nb=10)
+    t1 = time.time(); t_no_reuse = t1-t0
+    samples1.geometry = domain_geometry
+    
+    #%% 3.2. Set PDE2 to be a shallow copy of PDE1 but with different rhs
+    PDE1.reuse_assembled = True
+    PDE2 = PDE1.with_updated_rhs(poisson2.rhs_form)
+    cuqi_model2.pde = PDE2
+    
+    #%% 3.3. Sample the posterior again (Case 2: reuse of assembled operators)
+    np.random.seed(0)
+    sampler = cuqi.sampler.MetropolisHastings(cuqi_posterior)
+    t0 = time.time()
+    samples2 = sampler.sample_adapt(Ns,Nb=10)
+    t1 = time.time(); t_reuse = t1-t0
+
+    #%% 3.4. Check that the samples are the same
+    assert np.allclose(samples1.samples, samples2.samples)
+
+    #%% 3.5. Check that the samples matches the ones generated 
+    # before updating the library code to add the reuse_assembled functionality
+    samples_orig_file =\
+    copy_reference("data/samples_before_adding_reuse_assembled_feature.npz")
+    assert np.allclose(samples1.samples, np.load(samples_orig_file)["arr_0"][:,:Ns])
+    #%% 3.6. Check that the time is reduced
+    assert t_reuse < 0.7*t_no_reuse
+
+      
+class Poisson:
+    """Define the variational PDE problem for the Poisson equation in two
+    ways: as a full form, and as lhs and rhs forms"""
+
+    def __init__(self):
+
+        # Create the mesh and define function spaces for the solution and the
+        # parameter
+        self.mesh = dl.UnitIntervalMesh(10)
+
+        # Define the boundary condition
+        self.bc_value = dl.Constant(0.0)
+
+        # the source term
+        self.f = dl.Expression('x[0]', degree=1)
+
+    @property
+    def form(self):
+        return lambda m, u, v: dl.exp(m)*dl.inner(dl.grad(u), dl.grad(v))*dl.dx\
+            - self.f*v*dl.dx
+
+    @property
+    def lhs_form(self):
+        return lambda m, u, v: dl.exp(m)*dl.inner(dl.grad(u), dl.grad(v))*dl.dx
+
+    @property
+    def rhs_form(self):
+        return lambda m, v: self.f*v*dl.dx
+
+    @property
+    def solution_function_space(self):
+        return dl.FunctionSpace(self.mesh, "CG", 2)
+
+    @property
+    def parameter_function_space(self):
+        return dl.FunctionSpace(self.mesh, "CG", 1)
+
+    @property
+    def bc(self):
+        if not hasattr(self, "_bc") or self._bc is None:
+            self._bc = dl.DirichletBC(self.solution_function_space,
+                                      self.bc_value, "on_boundary")
+        return self._bc
+    
+    @bc.setter
+    def bc(self, bc):
+        self._bc = bc
