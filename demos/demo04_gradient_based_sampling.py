82c084b8fefc8560ad0dc46867b8e52351495edb
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-03-14 10:30:53 +0100

    set up without noise

--- demos/demo04_gradient_based_sampling.py
+++ demos/demo04_gradient_based_sampling.py
@@ -10,12 +10,13 @@ import ufl
 from scipy import optimize
 np.random.seed(seed=1)
 dl.set_log_level(40)
+import time
 
 #%% 1. Set up FEniCS PDE
 #%% 1.1. Set up mesh
 ndim = 1
-nx = 12
-ny = 12
+nx = 10
+ny = 10
 mesh = dl.UnitSquareMesh(nx, ny)
 
 #%% 1.2. Set up function spaces
@@ -36,7 +37,12 @@ adjoint_dirichlet_bc = dl.DirichletBC(solution_function_space,
                                       u_boundary) #adjoint problem bcs
 
 #%% 1.4. Set up source term
-f = dl.Constant(1.0)
+f_exp = dl.Constant(1.0)
+#f_exp = dl.Expression("4.0*exp(-pow((x[0]-c0)*s0,2))*exp(-pow((x[1]-c1)*s1,2))",c0=0.5, c1=.8, s0=10, s1=10, degree=1)
+f = dl.interpolate(f_exp, solution_function_space)
+
+im = dl.plot(f)
+plt.colorbar(im)
 
 #%% 1.5. Set up PDE variational form
 def form(m,u,p):
@@ -81,7 +87,7 @@ PDE = cuqipy_fenics.pde.SteadyStateLinearFEniCSPDE( form, mesh,
 
 #%% 2.2. Create domain geometry 
 fenics_continuous_geo = cuqipy_fenics.geometry.FEniCSContinuous(parameter_function_space)
-domain_geometry = cuqipy_fenics.geometry.MaternExpansion(fenics_continuous_geo, length_scale = .1, num_terms=32)
+domain_geometry = cuqipy_fenics.geometry.MaternExpansion(fenics_continuous_geo, length_scale = .1, num_terms=64, nu=0.1)
 
 #%% 2.3. Create range geometry
 range_geometry= cuqipy_fenics.geometry.FEniCSContinuous(solution_function_space)
@@ -94,16 +100,36 @@ cuqi_model = cuqi.model.PDEModel(PDE, domain_geometry =domain_geometry,range_geo
 #%% 2.5. Create exact solution and data and plot
 #%% 2.5. Create prior
 x = cuqi.distribution.Gaussian(mean=np.zeros(cuqi_model.domain_dim),
-                               cov=1, geometry=domain_geometry)
+                               cov=400, 
+                               geometry=domain_geometry)
 
 
 #%% 2.6. Create exact solution and data and plot
-exact_solution =cuqi.array.CUQIarray( np.random.randn(domain_geometry.par_dim),is_par=True,geometry= domain_geometry )
+exact_choice = 3
+if exact_choice == 1: # Prior sample
+    exact_solution =cuqi.array.CUQIarray( np.random.randn(domain_geometry.par_dim),is_par=True,geometry= domain_geometry )
+
+elif exact_choice == 2: # Custom signal
+    expr = dl.Expression("0.25*(sin(2*pi*x[0])+1.1)*(pow(r,2)<pow(x[0]-c0,2)+pow(x[1]-c1,2))+.1", r = 0.2, c0=0.6, c1=0.2, degree=1)
+    exact_fun = dl.interpolate(expr, parameter_function_space)
+    exact_solution =cuqi.array.CUQIarray( exact_fun,is_par=False,geometry= domain_geometry )
+    im = exact_solution.plot()
+    plt.colorbar(im[0])
+
+elif exact_choice == 3: #square-circle # Custom signal
+    expr = dl.Expression("7*(pow(r,2)>pow(x[0]-c0,2)+pow(x[1]-c1,2))+1+12*(x[0]<0.3)", r = 0.2, c0=0.8, c1=0.4, degree=1)
+    exact_fun = dl.interpolate(expr, parameter_function_space)
+    exact_solution =cuqi.array.CUQIarray( exact_fun,is_par=False,geometry= domain_geometry )
+    im = exact_solution.plot()
+    plt.colorbar(im[0])
+
+else:
+    raise ValueError('Exact choice not implemented')
 
 data = cuqi_model(exact_solution)
 
 #%% plot exact solution
-im = exact_solution.plot(vmin=-2, vmax=1)
+im = exact_solution.plot()#(vmin=-2, vmax=1)
 plt.colorbar(im[0])
 
 
@@ -139,17 +165,22 @@ plt.plot(scipy_grad , '--')
 
 
 #%% 2.10. Sample posterior
-Ns = 100
+Ns = 50
 #sampler = cuqi.sampler.MALA(cuqi_posterior, scale = 2/solution_function_space.dim(),x0=np.zeros(domain_geometry.dim))
+t0 = time.time()
 sampler = cuqi.sampler.NUTS(cuqi_posterior)
 samples = sampler.sample_adapt(Ns,Nb=10)
+t1 = time.time()
+print("Sampling time: ", t1-t0)
 
 #%% 2.11. Plot samples mean
 im =samples.plot_mean()
 plt.colorbar(im[0])
 
 #%% 2.12. Plot credible interval
-samples.plot_ci(plot_par=True,exact=exact_solution)
+samples.plot_ci(plot_par=True)#,exact=exact_solution)
 
 # %% 2.13. Plot trace
 samples.plot_trace()
+
+# %%
