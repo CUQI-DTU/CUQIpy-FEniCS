1fcc053734a8d08519182d621f69b7be4df8c5fb
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-04-17 06:38:42 +0200

    add utility function and update

--- cuqipy_fenics/utilities.py
+++ cuqipy_fenics/utilities.py
@@ -1,10 +1,33 @@
 import dolfin as dl
+from numbers import Number
 
 class ExpressionFromCallable(dl.UserExpression):
-    def __init__(self, callable, **kwargs):
+    def __init__(self, func, **kwargs):
         super().__init__(**kwargs)
-        self.callable = callable
+        self.func = func
 
     def eval(self, value, x):
-        value[:] = self.callable(x)
+        value[:] = self.func(x)
+
+
+def to_dolfin_expression(value, **expression_kwargs):
+    """Converts a value to a `dolfin.Expression`. The value should be a 
+    callable, `dolfin.Expression` or a number. expression_kwargs are passed to
+    the `dolfin.Expression` constructor if the value is a callable. An example 
+    of the keyword arguments is the `degree` argument for `dolfin.Expression` 
+    which is used to specify the element (polynomial) degree of the expression.
+    """
+
+    if not callable(value) and expression_kwargs is not {}:
+        raise ValueError("Cannot pass kwargs to non-callable value")
+    if callable(value):
+        return ExpressionFromCallable(value, **expression_kwargs)
+    elif isinstance(value, dl.UserExpression) \
+        or isinstance(value, dl.Expression)\
+            or isinstance(value, dl.Constant):
+        return value
+    elif isinstance(value, Number):
+        return dl.Constant(value)
+    else:
+        raise ValueError("Cannot convert to dolfin.Expression")
 
