441a0ed2fed7f14a9e24be09d407123fbe7bb3f8
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-04-19 05:16:25 +0200

    Add file for testing testproblem module

---
+++ tests/test_testproblem.py
@@ -0,0 +1,130 @@
+import dolfin as dl
+import cuqipy_fenics
+import numpy as np
+import pytest
+import matplotlib.pyplot as plt
+from cuqi.distribution import Gaussian
+from cuqi.geometry import Continuous1D
+
+@pytest.mark.parametrize("bc_types, bc_values, case", [
+    (None, None, "valid_1"),
+    (['Neumann', 'Dirichlet', 'dirichlet', 'neumann'],
+     [-2, lambda x: 0.9*np.sin(x[0])+1, 1, lambda x: x[0]+0.1],
+     "valid_2"),
+    (['Dirichlet', 'Dirichlet', 'Dirichlet', 'Dirichlet'],
+     [0, 3, 0, 1],
+     "valid_3"),
+    (['Neumann', 'Neumann', 'Neumann', 'neumann'],
+     [0, 1, 0, 1],
+     "all_neumann_error"),
+    (['Neumann', 'Dirichlet'],
+     [0, 1],
+     "missing_bc_error")])
+def test_FEniCSPoisson2D_bc(bc_types, bc_values, case):
+    """Test creating a FEniCSPoisson2D testproblem with different boundary conditions"""
+    if case[:5] == "valid":
+        testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D((20, 20), 
+                                    bc_types=bc_types, bc_values=bc_values, f=0)
+        
+        testproblem.data.plot()
+        testproblem.exactData.plot()
+        testproblem.exactSolution.plot()
+
+    if case == "all_neumann_error":
+        with pytest.raises(ValueError, match=r"All boundary conditions cannot be Neumann"):
+            testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D((20, 20), 
+                                        bc_types=bc_types, bc_values=bc_values, f=0)
+            
+    if case == "missing_bc_error":
+        with pytest.raises(ValueError, match=r"length of bc_type should be 4"):
+            testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D((20, 20), 
+                                        bc_types=bc_types, bc_values=bc_values, f=0)
+
+    # Check that the Dirichlet boundary conditions are sat up
+    #  correctly by comparing to the exact solution
+    if case == "valid_3":
+        func = testproblem.exactData.funvals
+        assert np.isclose(func(0, 0.5), bc_values[0]) 
+        assert np.isclose(func(0.5, 0), bc_values[1])
+        assert np.isclose(func(1, 0.5), bc_values[2])
+        assert np.isclose(func(0.5, 1), bc_values[3])
+
+    if case == "valid_2":
+        func = testproblem.exactData.funvals
+        assert np.isclose(func(0.5, 0), bc_values[1]([0.5, 0]))
+        assert np.isclose(func(1, 0.5), bc_values[2])
+
+    
+@pytest.mark.parametrize("f, valid", [
+    (None, True),
+    (3, True),
+    (lambda x: x[0], True),
+    (dl.Constant(4), True)])
+def test_FEniCSPoisson2D_f(f, valid):
+    """Test creating a FEniCSPoisson2D testproblem with different source terms"""
+    if valid:
+        testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D((30, 30), f=f)
+        
+        testproblem.data.plot()
+        testproblem.exactData.plot()
+        testproblem.exactSolution.plot()
+
+@pytest.mark.parametrize(
+    "exactSolution, relative_noise_std, field_type, field_params, mapping,"+\
+        "prior, case",
+    [(None, 0.02, None, None, None, None, "valid"),
+     (None, 0.02, None, None, None, None, "unknown_field"), #
+     (None, 0.05, None, None, "exponential", Gaussian(0, 1), "prior_name_error"),
+     (lambda x: x[0]+x[1]+0.1, 0.02, None, None, "exponential", Gaussian(0, np.ones(21*21), name='x'), "valid"),
+     (None, 0.05, None, None, lambda m: m+1, None, "valid"),
+     (None, 0.1, "KL", None, "exponential", None, "valid"),
+     (np.random.randn(10), 0.02, "KL", {'length_scale': 0.2, 'num_terms': 10}, "exponential", None, "valid")])                
+def test_FEniCSPoisson2D_setup(exactSolution, relative_noise_std,
+                               field_type, field_params, mapping,
+                               prior, case):
+    """Test creating a FEniCSPoisson2D testproblem with different 
+    parametrization, exact solution and noise level"""
+    if case == "valid":
+        testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D(
+            (20, 20),
+            exactSolution=exactSolution,
+            relative_noise_std=relative_noise_std,
+            field_type=field_type,
+            field_params=field_params,
+            mapping=mapping,
+            prior=prior)
+
+        testproblem.data.plot()
+        testproblem.exactData.plot()
+        testproblem.exactSolution.plot()
+
+    if case == "prior_name_error":
+        with pytest.raises(ValueError, match=r"Prior name is expected to be"):
+            testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D(
+                (20, 20),
+                exactSolution=exactSolution,
+                relative_noise_std=relative_noise_std,
+                field_type=field_type,
+                field_params=field_params,
+                mapping=mapping,
+                prior=prior)    
+            
+    if case == "unknown_field":
+        with pytest.raises(ValueError, match=r"Unknown field type"):
+            testproblem = cuqipy_fenics.testproblem.FEniCSPoisson2D(
+                (20, 20),
+                exactSolution=exactSolution,
+                relative_noise_std=relative_noise_std,
+                field_type=Continuous1D(21*21),
+                field_params=field_params,
+                mapping=mapping,
+                prior=prior)
+            
+    # Check the noise level is correct
+    if case == "valid":
+        noise_norm = np.linalg.norm(testproblem.data.funvals.vector().get_local()
+                                    - testproblem.exactData.funvals.vector().get_local())
+        exact_norm = np.linalg.norm(testproblem.exactData.funvals.vector().get_local())
+        assert np.isclose(noise_norm/exact_norm, relative_noise_std)
+            
+        
