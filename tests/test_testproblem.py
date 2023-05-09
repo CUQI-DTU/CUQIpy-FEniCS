ca407ad1149c4499d09538d1fd847934f9c53900
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-05-09 12:42:55 +0200

    update variable names

--- tests/test_testproblem.py
+++ tests/test_testproblem.py
@@ -26,7 +26,7 @@ def test_FEniCSPoisson2D_bc(bc_types, bc_values, case):
     """Test creating a FEniCSPoisson2D testproblem with different boundary conditions"""
     if case[:5] == "valid":
         testproblem = FEniCSPoisson2D((20, 20),
-                                      bc_types=bc_types, bc_values=bc_values, f=0)
+                                      bc_types=bc_types, bc_values=bc_values, source_term=0)
 
         testproblem.data.plot()
         testproblem.exactData.plot()
@@ -35,12 +35,12 @@ def test_FEniCSPoisson2D_bc(bc_types, bc_values, case):
     if case == "all_neumann_error":
         with pytest.raises(ValueError, match=r"All boundary conditions cannot be Neumann"):
             testproblem = FEniCSPoisson2D((20, 20),
-                                          bc_types=bc_types, bc_values=bc_values, f=0)
+                                          bc_types=bc_types, bc_values=bc_values, source_term=0)
 
     if case == "missing_bc_error":
         with pytest.raises(ValueError, match=r"length of bc_types list should be 4"):
             testproblem = FEniCSPoisson2D((20, 20),
-                                          bc_types=bc_types, bc_values=bc_values, f=0)
+                                          bc_types=bc_types, bc_values=bc_values, source_term=0)
 
     # Check that the Dirichlet boundary conditions are sat up
     #  correctly by comparing to the exact solution
@@ -57,15 +57,15 @@ def test_FEniCSPoisson2D_bc(bc_types, bc_values, case):
         assert np.isclose(func(1, 0.5), bc_values[2])
 
 
-@pytest.mark.parametrize("f, valid", [
+@pytest.mark.parametrize("source_term, valid", [
     (None, True),
     (3, True),
     (lambda x: x[0], True),
     (dl.Constant(4), True)])
-def test_FEniCSPoisson2D_f(f, valid):
+def test_FEniCSPoisson2D_source_term(source_term, valid):
     """Test creating a FEniCSPoisson2D testproblem with different source terms"""
     if valid:
-        testproblem = FEniCSPoisson2D((30, 30), f=f)
+        testproblem = FEniCSPoisson2D((30, 30), source_term=source_term)
 
         testproblem.data.plot()
         testproblem.exactData.plot()
@@ -73,7 +73,7 @@ def test_FEniCSPoisson2D_f(f, valid):
 
 
 @pytest.mark.parametrize(
-    "exactSolution, relative_noise_std, field_type, field_params, mapping," +
+    "exactSolution, noise_level, field_type, field_params, mapping," +
     "prior, case",
     [(None, 0.02, None, None, None, None, "valid"),
      (None, 0.02, None, None, None, None, "unknown_field"),
@@ -84,7 +84,7 @@ def test_FEniCSPoisson2D_f(f, valid):
      (None, 0.1, "KL", None, "exponential", None, "valid"),
      (np.random.randn(10), 0.02, "KL",
       {'length_scale': 0.2, 'num_terms': 10}, "exponential", None, "valid")])
-def test_FEniCSPoisson2D_setup(exactSolution, relative_noise_std,
+def test_FEniCSPoisson2D_setup(exactSolution, noise_level,
                                field_type, field_params, mapping,
                                prior, case):
     """Test creating a FEniCSPoisson2D testproblem with different 
@@ -93,7 +93,7 @@ def test_FEniCSPoisson2D_setup(exactSolution, relative_noise_std,
         testproblem = FEniCSPoisson2D(
             (20, 20),
             exactSolution=exactSolution,
-            relative_noise_std=relative_noise_std,
+            noise_level=noise_level,
             field_type=field_type,
             field_params=field_params,
             mapping=mapping,
@@ -108,7 +108,7 @@ def test_FEniCSPoisson2D_setup(exactSolution, relative_noise_std,
             testproblem = FEniCSPoisson2D(
                 (20, 20),
                 exactSolution=exactSolution,
-                relative_noise_std=relative_noise_std,
+                noise_level=noise_level,
                 field_type=field_type,
                 field_params=field_params,
                 mapping=mapping,
@@ -119,7 +119,7 @@ def test_FEniCSPoisson2D_setup(exactSolution, relative_noise_std,
             testproblem = FEniCSPoisson2D(
                 (20, 20),
                 exactSolution=exactSolution,
-                relative_noise_std=relative_noise_std,
+                noise_level=noise_level,
                 field_type=Continuous1D(21*21),
                 field_params=field_params,
                 mapping=mapping,
@@ -132,4 +132,4 @@ def test_FEniCSPoisson2D_setup(exactSolution, relative_noise_std,
                            - testproblem.exactData.funvals.vector().get_local())
         exact_norm = np.linalg.norm(
             testproblem.exactData.funvals.vector().get_local())
-        assert np.isclose(noise_norm/exact_norm, relative_noise_std)
+        assert np.isclose(noise_norm/exact_norm, noise_level)
