472221769d47bef87cdf5787023e2b33afc5e0a9
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-05-09 12:45:35 +0200

    add docstring to utility class

--- cuqipy_fenics/utilities.py
+++ cuqipy_fenics/utilities.py
@@ -2,6 +2,13 @@ import dolfin as dl
 from numbers import Number
 
 class ExpressionFromCallable(dl.UserExpression):
+    """A `dolfin.Expression` that is created from a callable. The callable
+    should take a single argument, the coordinate vector `x` and return a
+    number, the value of the expression at `x`. The keyword arguments are passed
+    to the `dolfin.Expression` constructor. An example of the keyword arguments 
+    is the `degree` argument for `dolfin.Expression` which is used to specify 
+    the element (polynomial) degree of the expression.
+    """
     def __init__(self, func, **kwargs):
         super().__init__(**kwargs)
         self.func = func
