8bf15d68f7bb9daad43d5a47dc29de6a15c7f209
amal-ghamdi <amal.m.alghamdi@gmail.com>
2024-09-11 14:45:34 +0200

    update documentation

--- cuqipy_fenics/config.py
+++ cuqipy_fenics/config.py
@@ -2,4 +2,6 @@
 
 
 UFL_LEGACY = False
-"""bool: A flag to indicate whether to use the UFL legacy module."""
+"""bool: A flag to indicate whether to use the UFL legacy module. For
+this setup to take effect, this flag needs to be set to the intended
+value before using any other module in CUQIpy-FEniCS."""
